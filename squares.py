4508754277dc32d05738e020e614edb1ea3e1cf7
jparedes <alfaro.pg@gmail.com>
2016-06-28 19:50:38 -0500

    Added even numbers of the list

--- squares.py
+++ squares.py
@@ -1,5 +1,6 @@
 #Using range() to make a list() of numbers
 
 print("List of numbers:\n"+str(list(range(1,50))))#This code will print a list of 10 numbers
-print("\nOdd numbers in the list:\n"+str(list(range(1,50,2))))
+print("\neven numbers:\n"+str(list(range(2,50,2))))#This code will print all even numbers in the list
+print("\nOdd numbers in the list:\n"+str(list(range(1,50,2))))#This code will print all odd numbers in the list
 
